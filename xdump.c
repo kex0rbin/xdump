8efc236536a1849d3cba71c5940c10aff61640b8
kex0rbin <175838531+kex0rbin@users.noreply.github.com>
2026-07-29 00:02:04 -0500

    Better offset, fixing bugs

--- xdump.c
+++ xdump.c
@@ -10,23 +10,22 @@
 #define GREEN "\x1b[32;1m"
 #define RESET "\x1b[0m"
 
-#define HELP_FILE "hx tool: Put the name of the file as the second argument. You can use arguments\n hx <file> <lines>"
-#define INVALID_ARGUMENTS "hx tool: To many arguments.\n"
-#define F_LINE "Offset\t00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F\t   Decoded Text\n\n"
-#define ERROR_FILE "hx tool: File not found.\n"
-#define INVALID_INPUT "hx tool: Invalid number! (Second argument).\n"
+#define HELP_FILE " xdmp tool: Put the name of the file as the second argument. Uses:\n \txdmp <file>\n \txdmp <file> <lines>\n"
+#define INVALID_ARGUMENTS " xdmp tool: To many arguments.\n"
+#define F_LINE "Offset\t\t00   01   02   03   04   05   06   07   08   09   0A   0B   0C   0D   0E   0F\tDecoded Text\n\n"
+#define ERROR_FILE " xdmp tool: File not found.\n"
+#define INVALID_INPUT " xdmp tool: Invalid number! (Second argument).\n"
 
 void print_string(char *str);
 void print_byte(int byte);
 void header(){
 	printf(F_LINE);
 }
+void show_all(FILE *f);
+void show_with_limits(FILE *p, int limit);
 
 int main(int argc, char *argv[]){
-	if(argc <= 1){
-		printf(HELP_FILE);
-		return 1;
-	}
+	if(argc <= 1){printf(HELP_FILE);return 1;}
 
 	if(argc >= 2){
 		if(argc > 3){
@@ -39,64 +38,69 @@ int main(int argc, char *argv[]){
 			printf(ERROR_FILE);
 			return 1;
 		}
-		char string[17];
-		int c, cont = 0;
-		long addr = ftell(f);
-
 
 		if(argc == 2){
-			header();
-			printf("%x\t", addr); 
-			while((c = fgetc(f)) != EOF)
-			{
-				string[cont] = intToChar(c);
-				print_byte(c);
-				cont++;
-				if(cont == 16){
-					string[16] = '\0';
-					print_string(string);
-					addr = ftell(f);
-					printf("\n");
-					printf("%x\t", addr);
-					cont = 0;
-				}
-			}
+			show_all(f);
 		}else{
-
 			int limit = atoi(argv[2]);
-			if(limit != 0 && limit > 0){
-				header();
-				printf("%x\t", addr); 
-				int lines = 0;
-				while((c = fgetc(f)) != EOF && lines <= limit)
-				{
-					string[cont] = intToChar(c);
-					print_byte(c);
-					cont++;
-					if(cont == 16){
-						string[16] = '\0';
-						print_string(string);
-						printf("\n");
-						lines++;
-						if(lines >= limit) break;
-						addr = ftell(f);
-						printf("%x\t", addr);
-						cont = 0;
-					
-					}
-				
-				}
+			if(limit > 0){
+				show_with_limits(f,limit);
 			}else{
 				printf(INVALID_INPUT);
 			}
-			
 		}
 		printf("\n");
 		fclose(f);
 		return 0;
 	}		
+}
 
-	return 0;
+void show_all(FILE *f){
+		fseek(f,0,SEEK_END);
+		long seek = ftell(f);
+		rewind(f);
+
+		char string[17];
+		int c, cont = 0;
+		header();
+		printf("%08x\t", ftell(f)); 
+		while((c = fgetc(f)) != EOF){
+				string[cont] = intToChar(c);
+				print_byte(c);
+				cont++;
+				if(cont == 16 || ftell(f) == seek){
+					string[cont] = '\0';
+					print_string(string);
+					printf("\n");
+					if(ftell(f) != seek) printf("%08x\t", ftell(f));
+					cont = 0;
+				}
+		}
+
+}
+
+void show_with_limits(FILE *f, int limit){
+		char string[17];
+		int c, cont = 0;
+		header();
+		printf("%08x\t", ftell(f)); 
+		int lines = 0;
+		while((c = fgetc(f)) != EOF && lines <= limit){
+			string[cont] = intToChar(c);
+			print_byte(c);
+			cont++;
+			if(cont == 16){
+				string[cont] = '\0';
+				print_string(string);
+				printf("\n");
+				lines++;
+				if(lines >= limit) break;
+				printf("%08x\t", ftell(f));
+				cont = 0;	
+			}
+				
+		}
+
 }
 
 void print_string(char *str){
@@ -112,7 +116,7 @@ void print_string(char *str){
 }
 
 void print_byte(int byte){
-	if(byte >= 40 && byte <= 122){
+	if(byte >= 40 && byte <= 125){
 		printf(GREEN "%02x   " RESET, byte);
 	}else{
 		printf("%02x   ", byte);
